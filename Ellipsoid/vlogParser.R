c5f5ab4f4f18b4de4c6c370ec287699bec8c1ec8
Alexey Bochkarev <aabochkaryov@gmail.com>
2018-12-08 01:34:15 -0500

    made a step-by-step figure for ellipsoid algo

--- Ellipsoid/vlogParser.R
+++ Ellipsoid/vlogParser.R
@@ -3,27 +3,47 @@ library(stringr)
 library(ggplot2)
 library(car)
 library(dplyr)
+library(ggrepel)
+library(gridExtra)
 
-showEllipse <- function(H,c,Mx,My,p0,s){
+showEllipse <- function(H,c,xmin,xmax,ymin,ymax,p0,s,col="red"){
   ell = ellipse(draw = FALSE, center = c, shape = as.matrix(H), radius = 1,segments=s);
   if(is.null(ell) || length(ell)==0) return(p0);
 
-  ell = ell[(abs(ell[,1])<= Mx) & (abs(ell[,2])<=My),];
+  ell = ell[(ell[,1]<=xmax) & (ell[,1]>=xmin) & (ell[,2]<=ymax) & (ell[,2]>=ymin),];
 
   p = p0;
   if(!is.null(ell) && length(ell)>2){
     df = data.frame(X = ell[,1], Y = ell[,2]);
-    p <- p+geom_point(data=df,aes(x=X,y=Y), color="red",size=0.1);
+    p <- p+geom_point(data=df,aes(x=X,y=Y), color=col,size=0.1)+xlim(xmin,xmax)+ylim(ymin,ymax);
   };
 
-  if(abs(c[1])<=Mx & abs(c[2])<=My){
+  if(c[1]<=xmax & c[1]>=xmin & c[2]<=ymax & c[1]>=ymin){
     df = data.frame(X = c[1], Y = c[2]);
-    p <- p + geom_point(data=df,aes(x=X,y=Y), color="red",size=2);
+    p <- p + geom_point(data=df,aes(x=X,y=Y), color=col,size=2);
   }
 
   return(p);
 }
 
+showHyperplane <- function(w,o,p,col="black",...){
+  df = data.frame();
+
+  gr = p;
+
+  if(abs(w[2])>0){
+    slope = - w[1] / w[2];
+    intr = sum(o*w)/w[2];
+    gr = gr + geom_abline(slope = slope, intercept = intr,color=col,...)
+  }
+
+  if(abs(w[2])==0 & abs(w[1])>0){
+    intr = sum(o*w)/w[2];
+    gr = gr + geom_abline(xintercept = intr, color=col,...);
+  }
+
+  return(gr);
+}
 
 mydata = read_file("logs/2d-for-pic.log")
 
@@ -72,7 +92,8 @@ for(i in 1:nrow(A)){
 
 
 
-## get some ellipses
+## get some ellipses:
+
 
 for (i in 1:100){
   f = showEllipse(as.matrix(H[[i]]),C[[i]]$V1,Mx,My,p,500)
@@ -96,6 +117,45 @@ abs(ell[,1])<=10 & abs(ell[,2]) <=10
 ## Figure preparation for the Ellipsoid algorithm introduction
 
 dfp = data.frame(X = c(1,2,4,5),Y = c(1,2,2,1));
+dfp$txt = paste("(",dfp$X,",",dfp$Y,")",sep="")
 
-ggplot(dfp)+
-  geom_polygon(aes(x=X,y=Y),fill="blue", )
+g = ggplot(dfp)+
+  geom_polygon(aes(x=X,y=Y),fill="blue", alpha=0.2)+
+  geom_polygon(aes(x=X,y=Y),color="blue",fill=NA,size=1)+
+  geom_point(aes(x=X,y=Y), color="blue", size=5)+
+  theme(
+    plot.title = element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
+
+## add some isolines
+
+for(i in -3:7)
+  g=showHyperplane(c,c(i,0),g,col="grey75",linetype="dashed")
+
+
+showPic <- function(i,g,xmin=-2,xmax=7.5,ymin=-0.5,ymax=3.5,s=500,title=""){
+  p=showEllipse(H[[i-1]],C[[i-1]]$V1,xmin,xmax,ymin,ymax,g,100,col="grey25")
+  p=showEllipse(H[[i]],C[[i]]$V1,xmin,xmax,ymin,ymax,p,s)
+  showHyperplane(wt[[i-1]]$V1,C[[i-1]]$V1,p)+
+    ggtitle(paste(title," -- Step: ",i,sep=""))+
+    theme(plot.title = element_text(size = 10, face = "bold",hjust = 0, vjust = 0.5))
+}
+
+
+png("ell-steps.png",width = 7, height = 10, units = "in",res=600)
+grid.arrange(
+  grobs = list(showPic(51,g,s=1000,title="A"),
+               showPic(52,g,s=1000,title="B"),
+               showPic(53,g,s=1000,title="C"),
+               showPic(54,g,s=1000,title="D"),
+               showPic(65,g,s=1000,title="E"),
+               showPic(70,g,s=1000,title="F"),
+               showPic(75,g,s=1000,title="G"),
+               showPic(80,g,s=1000,title="H")),
+  widths = c(1, 1),
+  layout_matrix = rbind(c(1, 2),
+                        c(3, 4),
+                        c(5, 6),
+                        c(7,8)))
+dev.off()
